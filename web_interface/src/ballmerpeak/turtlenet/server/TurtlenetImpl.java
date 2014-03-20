95c7618d36d37a2f75972ffefed9de84185315f0
thomal <luke@luke-thomas.net>
2014-03-20 12:58:33 +0000

    fixed GWT

--- web_interface/src/ballmerpeak/turtlenet/server/TurtlenetImpl.java
+++ web_interface/src/ballmerpeak/turtlenet/server/TurtlenetImpl.java
@@ -4,14 +4,18 @@ import ballmerpeak.turtlenet.client.Turtlenet;
 import ballmerpeak.turtlenet.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+/**
+ * The server-side implementation of the RPC service.
+ */
 @SuppressWarnings("serial")
 public class TurtlenetImpl extends RemoteServiceServlet implements Turtlenet {
 
-  public String greetServer(String input) throws IllegalArgumentException { 
+  public String greetServer(String input) throws IllegalArgumentException {
     if (!FieldVerifier.isValidName(input)) {
-      throw new IllegalArgumentException("Name must be at least 4 characters long");
+      throw new IllegalArgumentException(
+          "Name must be at least 4 characters long");
     }
 
-    return "Hello, " + input;
+    return "Ignoring: \"" + input + "\"";
   }
 }
