d8dcc8e279b861028e0471e4900f392bc6f5bc17
thomal <luke@luke-thomas.net>
2014-03-20 13:48:16 +0000

    test fileio

--- web_interface/src/ballmerpeak/turtlenet/server/TurtlenetImpl.java
+++ web_interface/src/ballmerpeak/turtlenet/server/TurtlenetImpl.java
@@ -3,19 +3,24 @@ package ballmerpeak.turtlenet.server;
 import ballmerpeak.turtlenet.client.Turtlenet;
 import ballmerpeak.turtlenet.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import java.io.*;
 
-/**
- * The server-side implementation of the RPC service.
- */
 @SuppressWarnings("serial")
 public class TurtlenetImpl extends RemoteServiceServlet implements Turtlenet {
 
-  public String test(String input) throws IllegalArgumentException {
-    if (!FieldVerifier.isValidName(input)) {
-      throw new IllegalArgumentException(
-          "Command must be at least 4 characters long");
-    }
+    public String test(String input) throws IllegalArgumentException {
+        if (!FieldVerifier.isValidName(input))
+            throw new IllegalArgumentException("Command must be at least 4 characters long");
 
-    return "Ignoring: \"" + input + "\"";
-  }
+        try {
+            File lastReadFile = new File("./db/lastread");
+                        
+            BufferedWriter writer = new BufferedWriter(new FileWriter("./TESTFILE"));
+            writer.write(input + "\n");
+            writer.close();
+            return "Success";
+        } catch (Exception e) {
+            return "ERROR: Unable to save file: " + e.toString();
+        }
+    }
 }
