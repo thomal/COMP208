ca4e2c46d46bc94b247eb261d306e08865b37a2c
louisprince <lprince.tds@gmail.com>
2014-03-28 13:48:55 +0000

    Started to rewrite the interface in the proper GWT style

--- src/ballmerpeak/turtlenet/client/frontend.java
+++ src/ballmerpeak/turtlenet/client/frontend.java
@@ -16,142 +16,39 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.cellview.client.Header;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.History;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
-public class frontend implements EntryPoint {
-    private static final String SERVER_ERROR  = "An error occurred while " +
-                 "attempting to contact the server. Please check Turtlenet is running.";
-    private static final String RSERVER_ERROR = "An error occurred while " +
-                 "attempting to contact the remote server.";
+public class frontend implements EntryPoint{
 
     /**
      * Create a remote service proxy to talk to the server-side Turtlenet service.
      */
     private final TurtlenetAsync turtlenet = GWT.create(Turtlenet.class);
+    
+    HorizontalPanel navigation = new HorizontalPanel();
 
-    /**
-     * This is the entry point method.
-     */
     public void onModuleLoad() {        
-//PAGE SETUP----------------------------------------------------------------
-        final Button sendButton = new Button("Send");
-        final TextBox nameField = new TextBox();
-        nameField.setText("");
-        final Label errorLabel = new Label();
-
-        // We can add style names to widgets
-        sendButton.addStyleName("sendButton");
-
-        // Add the nameField and sendButton to the RootPanel
-        // Use RootPanel.get() to get the entire body element
-        RootPanel.get("nameFieldContainer").add(nameField);
-        RootPanel.get("sendButtonContainer").add(sendButton);
-        RootPanel.get("errorLabelContainer").add(errorLabel);
-
-        // Focus the cursor on the name field when the app loads
-        nameField.setFocus(true);
-        nameField.selectAll();
-
-        // Create the popup dialog box
-        final DialogBox dialogBox = new DialogBox();
-        dialogBox.setText("Remote Procedure Call");
-        dialogBox.setAnimationEnabled(true);
-        final Button closeButton = new Button("Close");
-        // We can set the id of a widget by accessing its Element
-        closeButton.getElement().setId("closeButton");
-        final Label textToServerLabel = new Label();
-        final HTML serverResponseLabel = new HTML();
-        VerticalPanel dialogVPanel = new VerticalPanel();
-        dialogVPanel.addStyleName("dialogVPanel");
-        dialogVPanel.add(new HTML("<b>Sending command to the server:</b>"));
-        dialogVPanel.add(textToServerLabel);
-        dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-        dialogVPanel.add(serverResponseLabel);
-        dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-        dialogVPanel.add(closeButton);
-        dialogBox.setWidget(dialogVPanel);
-
-        // Add a handler to close the DialogBox
-        closeButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                dialogBox.hide();
-                sendButton.setEnabled(true);
-                sendButton.setFocus(true);
-            }
-        });
-
-
-        // Create a handler for the sendButton and nameField
-        class MyHandler implements ClickHandler, KeyUpHandler {
-            /**
-             * Fired when the user clicks on the sendButton.
-             */
-            public void onClick(ClickEvent event) {
-                sendNameToServer();
-            }
-
-            /**
-             * Fired when the user types in the nameField.
-             */
-            public void onKeyUp(KeyUpEvent event) {
-                if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    sendNameToServer();
-                }
-            }
-
-            /**
-             * Send the name from the nameField to the server and wait for a response.
-             */
-            private void sendNameToServer() {
-                // First, we validate the input.
-                errorLabel.setText("");
-                String textToServer = nameField.getText();
-                if (!FieldVerifier.isValidName(textToServer)) {
-                    errorLabel.setText("Please enter at least four characters");
-                    return;
-                }
-                
-                // Then, we send the input to the server.
-                sendButton.setEnabled(false);
-                textToServerLabel.setText(textToServer);
-                serverResponseLabel.setText("");
-                turtlenet.test(textToServer, new AsyncCallback<String>() {
-                    public void onFailure(Throwable caught) {
-                        // Show the RPC error message to the user
-                        dialogBox.setText("Remote Procedure Call - Failure");
-                        serverResponseLabel.addStyleName("serverResponseLabelError");
-                        serverResponseLabel.setHTML(SERVER_ERROR);
-                        dialogBox.center();
-                        closeButton.setFocus(true);
-                    }
-
-                    public void onSuccess(String result) {
-                        dialogBox.setText("Remote Procedure Call");
-                        serverResponseLabel.removeStyleName("serverResponseLabelError");
-                        serverResponseLabel.setHTML(result);
-                        dialogBox.center();
-                        closeButton.setFocus(true);
-                    }
-                });
-            }
-        }
-
-        // Add a handler to send the name to the server
-        MyHandler handler = new MyHandler();
-        sendButton.addClickHandler(handler);
-        nameField.addKeyUpHandler(handler);
-        
-        //have local server connect to remote server
-        turtlenet.startTN(new AsyncCallback<String>() {
-            public void onFailure(Throwable caught) {
-                //
-            }
-
-            public void onSuccess(String result) {
-                //
-            }
-        });
-    }
+		//Create Navigation Links
+		Hyperlink linkWall = new Hyperlink("Wall", "wall");
+		Hyperlink linkMessages = new Hyperlink("Messages", "messages");
+		Hyperlink linkFriends = new Hyperlink("Friends", "friends");
+		Hyperlink linkEvents = new Hyperlink("Events", "events");
+        //Add links to navigation panel
+        navigation.add(linkWall);
+        navigation.add(linkMessages);
+        navigation.add(linkFriends);
+        navigation.add(linkEvents);
+        //Add style name to navigation for CSS
+        navigation.addStyleName("gwt-navigation");
+        //Add navigation to page
+        RootPanel.get().add(navigation);
+	}
 }
