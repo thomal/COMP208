f5441a5b18754afaa96daf4a58655c8385514c4f
thomal <luke@luke-thomas.net>
2014-04-08 09:51:47 +0100

    removed GUI class

--- src/ballmerpeak/turtlenet/server/TNClient.java
+++ src/ballmerpeak/turtlenet/server/TNClient.java
@@ -7,20 +7,16 @@ class TNClient implements Runnable {
         NetworkConnection connection    = new NetworkConnection("localhost");
         Thread            networkThread = new Thread(connection);
         Database          db            = new Database("./db");
-        //GUI               gui           = new GUI(db, connection);
-        //Thread            guiThread     = new Thread(gui);
         
         if (!Crypto.keysExist()) //move into GUI class
             Crypto.keyGen();
         
         networkThread.start(); //download new messages, wait 1 second, repeat
-        //guiThread.start();     //display content of DB, get user input, repeat
         
         while (running)
             while (connection.hasMessage())
                 Parser.parse(Crypto.decrypt(connection.getMessage()), db);
         
-        //gui.close();
         connection.close();
         db.dbDisconnect();
     }
