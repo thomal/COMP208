74609b0aa29432f4c7f96278b45f4d83e049c108
thomal <luke@luke-thomas.net>
2014-04-12 23:45:29 +0100

    tests for message::XgetY() methods, also explanatory note for louis re: global IDs

--- src/ballmerpeak/turtlenet/testing/Test.java
+++ src/ballmerpeak/turtlenet/testing/Test.java
@@ -19,14 +19,58 @@ class Test {
         System.out.println("===========================================================================");
     }
     
-    private static void test (int i, boolean v) {
+    private static void test (String i, boolean v) {
         if (v) {
-            System.out.println("\t" + i + " PASS");
+            System.out.println("\t" + i + getTabs(i) + "PASS");
             passes++;
         } else {
-            System.out.println("\t" + i + " FAIL");
+            System.out.println("\t" + i + getTabs(i) + "FAIL");
             failures++;
         }
+    }
+    
+    private static void test (String i, String answer, String expected) {
+        if (answer.equals(expected)) {
+            System.out.println("\t" + i + getTabs(i) + "PASS");
+            passes++;
+        } else {
+            System.out.println("\t" + i + getTabs(i) + "FAIL");
+            System.out.println("\t\tGot: " + answer);
+            System.out.println("\t\tExpected: " + expected);
+            failures++;
+        }
+    }
+    
+    private static void test (String i, long answer, long expected) {
+        if (answer == expected) {
+            System.out.println("\t" + i + getTabs(i) + "PASS");
+            passes++;
+        } else {
+            System.out.println("\t" + i + getTabs(i) + "FAIL");
+            System.out.println("\t\tGot: " + answer);
+            System.out.println("\t\tExpected: " + expected);
+            failures++;
+        }
+    }
+    
+    private static String getTabs (String s) {
+        int tabs = 0;
+        String ts = "";
+        
+        if (s.length() < 8)
+            tabs = 4;
+        else if (s.length() < 16)
+            tabs = 3;
+        else if (s.length() < 24)
+            tabs = 2;
+        else if (s.length() < 32)
+            tabs = 1;
+        else
+            tabs = 0;
+        
+        while (tabs-- > 0)
+            ts += "\t";
+        return ts;
     }
     
     private static boolean testMessageContructors() {
@@ -35,10 +79,10 @@ class Test {
         
         Message m = new Message("POST", "hello, world!\\_O_/", 4224, "<sig>");
         
-        test (1, m.getCmd().equals("POST"));
-        test (2, m.getSig().equals("<sig>"));
-        test (3, m.getContent().equals("hello, world!\\_O_/"));
-        test (4, m.getTimestamp() == 4224);
+        test ("1 getCmd"      , m.getCmd(),      "POST");
+        test ("2 getSig"      , m.getSig(),      "<sig>");
+        test ("3 getContent"  , m.getContent(),  "hello, world!\\_O_/");
+        test ("4 getTimestamp", m.getTimestamp(), 4224);
         
         
         return failures == ifailures;
@@ -50,10 +94,10 @@ class Test {
         
         Message m = Message.parse("abcd\\efgh\\ijkl\\mnop\\4224");
         
-        test (1, m.getCmd().equals("abcd"));
-        test (2, m.getSig().equals("efgh"));
-        test (3, m.getContent().equals("ijkl\\mnop"));
-        test (4, m.getTimestamp() == 4224);
+        test ("1 getCmd"      , m.getCmd(),       "abcd");
+        test ("2 getSig"      , m.getSig(),       "efgh");
+        test ("3 getContent"  , m.getContent(),   "ijkl\\mnop");
+        test ("4 getTimestamp", m.getTimestamp(),  4224);
         
         
         return failures == ifailures;
@@ -78,38 +122,37 @@ class Test {
         Message likem   = new Message("LIKE",   "<post or comment sig>",      4224, "<sig>");
         Message eventm  = new Message("EVNT",   "0:60000:bobs birthday",      4224, "<sig>");
         
-        test(1, claimm.CLAIMgetName().equals("zero_cool"));
+        test("1  CLAIMgetName", claimm.CLAIMgetName(), "zero_cool");
         
-        test(2, revokem.REVOKEgetTime() == 2442);
-        System.out.println("\tWARNING: Not testing Message::REVOKEgetKey()");
-        anomalies++;
+        test("2  REVOKEgetTime", revokem.REVOKEgetTime(), 2442);
         
         String[][] pdvs = pdatam.PDATAgetValues();
-        test(3, pdvs[0][0].equals("name"));
-        test(4, pdvs[0][0].equals("John Doe"));
-        test(5, pdvs[0][0].equals("dob"));
-        test(6, pdvs[0][0].equals("1972"));
+        test("3  PDATAgetValues", pdvs[0][0], "name");
+        test("4  PDATAgetValues", pdvs[0][0], "John Doe");
+        test("5  PDATAgetValues", pdvs[0][0], "dob");
+        test("6  PDATAgetValues", pdvs[0][0], "1972");
         
-        test(7, chatm.CHATgetKeys()[0].equals(k1));
-        test(8, chatm.CHATgetKeys()[1].equals(k2));
+        test("7  CHATgetKeys", chatm.CHATgetKeys()[0].equals(k1));
+        test("8  CHATgetKeys", chatm.CHATgetKeys()[1].equals(k2));
         
-        test(9, pchatm.PCHATgetText().equals("Hi bob."));
-        test(10, pchatm.PCHATgetConversationID().equals("<convsig>"));
+        test("9  PCHATgetText",  pchatm.PCHATgetText(),           "Hi bob.");
+        test("10 PCHATgetConversationID", pchatm.PCHATgetConversationID(), "<convsig>");
         
-        test(11, postm.POSTgetText().equals("Hello, World! \\_O_/"));
+        test("11 POSTgetText", postm.POSTgetText(), "Hello, World! \\_O_/");
         
         //No FPOSTgetX() methods because FPOSTs are POSTs in disguise.
         
-        test(12, cmntm.CMNTgetText().equals("nice"));
-        test(13, cmntm.CMNTgetItemID().equals("<post or comment sig>"));
+        test("12 CMNTgetText",   cmntm.CMNTgetText(),   "nice");
+        test("13 CMNTgetItemID", cmntm.CMNTgetItemID(), "<post or comment sig>");
         
-        test(14, likem.LIKEgetItemID().equals("<post or comment sig>"));
+        test("14 LIKEgetItemID", likem.LIKEgetItemID(), "<post or comment sig>");
         
-        test(15, eventm.EVNTgetName().equals("bobs birthday"));
-        test(16, eventm.EVNTgetStart() == 0);
-        test(17, eventm.EVNTgetEnd() == 60000);
-        
-        
+        test("15 EVNTgetName",  eventm.EVNTgetName(), "bobs birthday");
+        test("16 EVNTgetStart", eventm.EVNTgetStart(), 0);
+        test("17 EVNTgetEnd",   eventm.EVNTgetEnd(),   60000);
+ 
+        System.out.println("\tWARNING: Not testing Message::REVOKEgetKey()");
+        anomalies++;              
         
         return failures == ifailures;
     }
