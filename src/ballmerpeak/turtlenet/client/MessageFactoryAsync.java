faec99f723ceb20a7ef6dd399174a4f0baac251f
thomal <luke@luke-thomas.net>
2014-04-24 10:06:01 +0100

    new fpost field

--- src/ballmerpeak/turtlenet/client/MessageFactoryAsync.java
+++ src/ballmerpeak/turtlenet/client/MessageFactoryAsync.java
@@ -11,7 +11,7 @@ public interface MessageFactoryAsync {
     void newCHAT   (String[] keys,                        AsyncCallback<Message> callback);
     void newPCHAT  (String convoSig, String msg,          AsyncCallback<Message> callback);
     void newPOST   (String msg,                           AsyncCallback<Message> callback);
-    void newFPOST  (String msg,                           AsyncCallback<Message> callback);
+    void newFPOST  (String msg, String key,               AsyncCallback<Message> callback);
     void newCMNT   (String itemSig, String comment,       AsyncCallback<Message> callback);
     void newLIKE   (String itemSig,                       AsyncCallback<Message> callback);
     void newEVNT   (long start, long end, String descrip, AsyncCallback<Message> callback);
