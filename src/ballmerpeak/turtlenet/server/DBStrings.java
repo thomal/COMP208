c8074837a18da9cd31cf846302463f3135ba3987
Michael Chadwick <sgmchadw@liverpool.ac.uk>
2014-04-24 19:44:33 +0100

    Ported Current SQL to DBStrings.java
    I've left comments where there are things that need doing in java, and
    wiped the SQLstatements files as I went, so if any more SQL appears
    there we'll know it's new

--- src/ballmerpeak/turtlenet/server/DBStrings.java
+++ src/ballmerpeak/turtlenet/server/DBStrings.java
@@ -136,90 +136,156 @@ class DBStrings {
     };
     
     public static final String addClaim =
-        "INSERT INTO message_claim (claimID, username, signature)  "+
-        "VALUES ("+
-        "        NULL,"+
-        "        'userVar',"+
-        "        'sigVar'"+
-        "        );";
+
+        "INSERT INTO tbl_message_claim (message_claim_id, username, signature) "
+        + "VALUES (null, 'userVar', 'sigVar'); " ;
 
     public static final String getClaims =
-        "SELECT username, signature"+
-        "FROM message_claim;";
+        "SELECT username, signature "+
+        "FROM tbl_message_claim;" ;
 
-    public static final String getUsername =
-        "SELECT username"+
-        "FROM message_claim"+
-        "WHERE signature = 'sigVar'";
+    public static final String getUsername() {
+        //I have to JAVA here at some point
+    }
 
     public static final String addRevocation =
-        "INSERT INTO key_revoke (revokeID, signature)"+
-        "VALUES ("+
-                "NULL,"+
-                "'sigVar'"+
-                ");";
+        "INSERT INTO tbl_key_revoke (revoke_id, signature) " +
+        "VALUES (null, 'sigVar'); " ;
 
     public static final String getRevocations =
-        "SELECT signature, time"+
-        "FROM key_revoke";
+        "SELECT signature, time " +
+        "FROM tbl_key_revoke;" ;
 
     public static final String getTimedRevocations =
-        "SELECT signature"+
-        "FROM key_revoke"+
-        "WHERE time >= DATETIME('timeBeginVar')"+ //MIKE_IS_HERE
-        "AND time <= DATETIME('2014-12-31 00:00:00')";
+        "SELECT signature "+
+        "FROM tbl_key_revoke " +
+        "WHERE time >= DATETIME('1YYY-MM-DD HH:MM:SS') " +
+        "AND time <= DATETIME('2YYY-MM-DD HH:MM:SS'); " ;
 
 
-    public static final String isRevoked =
-        "INSERT INTO key_revoke (signature)"+
-        "VALUES ("+
-        "        'signature'"+
-        "        );";
+    public static final String isRevoked() {
+        //Need to do more JAVA Magic here
+    }
 
     public static final String addPData =
-        "INSERT INTO user (username, name, birthday, sex, email)"+
-        "VALUES ("+
-        "        'username',"+
-        "        'name',"+
-        "        'YYYY-MM-DD',"+
-        "        'genderChar'"+ //-- drop down option preferable F - female, M - male
-        "        'email@email.com'"+
-        "        );";
+        "INSERT INTO tbl_user (user_id, username, name, birthday, sex, " +
+        "email, public_key) " +
+        "VALUES (null, 'userVar', 'nameVar', 'YYYY-MM-DD', 'genderVar', 'email@email.com', 'keyVar');" ;
 
-    public static final String addChat = "";
-
-    public static final String addToChat = "";
-
-    public static final String addPost = "";
-
-    public static final String getPosts = "";
-
-    public static final String addIndependentComment = "";
-
-    public static final String addRelatedComment = "";
-
-    public static final String getComments = "";
+    public static final String createChat =
+        "INSERT INTO tbl_private_message (message_id, user_from, content) " +
+        "VALUES (null, 'userVar', 'contentVar');" ;
     
-    public static final String addLike = "";
+    public static final String getChat() {
+        //empty in the SQLstatements.sql file - assume more JAVA goes here
+    }
 
-    public static final String getPostLikes = "";
+    public static final String addToChat = 
+        "INSERT INTO tbl_is_in_message (is_in_id, message_id, user_id) " +
+        "VALUES (NULL, 'messageVar', 'userVar');" ;
 
-    public static final String getCommentLikes = "";
+    public static final String addPost =
+        "INSERT INTO tbl_wall_post (wall_id, user_from, user_to, " +
+                                   "content, signature) " +
+        "VALUES (NULL, 'userVarFrom', 'userVarTo', 'contentVar', 'sigVar');" ;
 
-    public static final String addEvent = "";
+    public static final String getPosts = 
+        "SELECT user_from, user_to, content " +
+        "FROM wall_post " +
+        "WHERE wall_id = 'wallVar';" ;
 
-    public static final String getEvent = "";
+    public static final String addIndependentComment = 
+        "INSERT INTO tbl_has_comment (comment_id, comment_content, " +
+                                     "wall_id, user_id, signature, " +
+                                     "comment_comment_id) " +
+        "VALUES (NULL, 'commentVar', 'wallVar', 'userVar', 'sigVar', " +
+        "'comment2Var');" ;
 
-    public static final String getEventWithInvites = "";
+    public static final String addRelatedComment =
+        //There wasn't any SQL for related comments yet
+        "";
 
-    public static final String acceptEvent = "";
+    public static final String getComments = 
+        "SELECT username, name, comment_content, time " +
+        "FROM tbl_user " +
+        "INNER JOIN tbl_has_comment " +
+        "ON tbl_user.user_id = tbl_has_comment.user_id " +
+        "WHERE wall_id = 'wallVar';" ;
+    
+    public static final String addLike = 
+        "INSERT INTO tbl_has_like (like_id, post_id, user_id, comment_id) " +
+        "VALUES (null, 'postVar', 'userVar', 'commentVar');" ;
 
-    public static final String declineEvent = "";
+    public static final String getPostLikes = 
+        "SELECT COUNT(like_id) " +
+        "FROM tbl_has_like " +
+        "WHERE wall_id = 'wallVar';" ;
 
-    public static final String addKey = "";
+    public static final String getCommentLikes = 
+        "SELECT COUNT(like_id) " +
+        "FROM tbl_has_like " +
+        "WHERE comment_id = 'commentVar';" ;
 
-    public static final String getKey = "";
+    public static final String addEvent = 
+        "INSERT INTO tbl_events (event_id, user_from, title, " +
+                                "content, start_date, end_date) " +
+        "VALUES (null, 'userVar', 'titleVar', 'contentVar', " +
+        "'1YYY-MM-DD', '2YYY-MM-DD');" ;
 
-    public static final String getName = "";
+    public static final String getEvent = 
+        "SELECT tbl_user.username, tbl_user.name, tbl_events.title, " +
+        "tbl_events.content, tbl_events.time, tbl_events.start_date, " +
+        "tbl_events.end_date, tbl_events.user_from " +
+        "FROM tbl_user " +
+        "INNER JOIN tbl_events " +
+        "ON tbl_user.user_id = tbl_events.user_from " +
+        "WHERE event_id = 'eventVar';" ;
+
+    public static final String getEventWithInvites = 
+        "SELECT tbl_events.title, tbl_events.content, tbl_events.time, " +
+        "tbl_events.start_date, tbl_events.end_date, tbl_events.from, " +
+        "tbl_user.username, tbl_user.name " +
+        "FROM tbl_events " +
+        "INNER JOIN tbl_is_invited " +
+        "ON tbl_events.event_id = tbl_is_invited.event_id " +
+        "INNER JOIN tbl_user " +
+        "ON tbl_is_invited.user_id = tbl_user.user_id " +
+        "INNER JOIN tbl_is_in_category " +
+        "ON tbl_user.user_id = tbl_is_in_category.user_id " +
+        "INNER JOIN tbl_category " +
+        "ON tbl_is_in_category.category_id = tbl_category.category_id " +
+        "WHERE event_id = 'eventVar';" ;
+
+    public static final String acceptEvent = 
+        "UPDATE tbl_is_invited " +
+        "SET decision = '1' " +
+        "WHERE user_id = 'userVar';" ;
+
+    public static final String declineEvent = 
+        "UPDATE tbl_is_invited " +
+        "SET decision = '0' " +
+        "WHERE user_id = 'userVar';" ;
+
+    public static final String addKey = 
+        //Empty at SQLstatements.sql - assuming java goes here
+        "";
+
+    public static final String getKey = 
+        "SELECT public_key " +
+        "FROM tbl_user " +
+        "WHERE username = 'userVar';" ;
+
+    public static final String getName = 
+        "SELECT username, name " +
+        "FROM tbl_user " +
+        "WHERE public_key = 'keyVar';" ;
+
+    public static final String addCategory =
+        "INSERT INTO tbl_category (category_id, name) " +
+        "VALUES (null, 'nameVar');" ;
+
+    public static final String addToCategory =
+        "INSERT INTO tbl_is_in_category (is_in_id, category_id, user_id) " +
+        "VALUES (null, 'categoryVar', 'userVar');" ;
 
 }
--- src/ballmerpeak/turtlenet/server/SQLstatements.sql
+++ src/ballmerpeak/turtlenet/server/SQLstatements.sql
@@ -2,145 +2,96 @@
 -- aishahsenin@gmail.com, 07984656781
 
 -- addClaim()
-INSERT INTO tbl_message_claim (message_claim_id, username, signature)
-VALUES (null, 'userVar', 'sigVar');
+--Ported
 
 -- getClaims()
-SELECT username, signature
-FROM tbl_message_claim;
+--Ported
 
 -- getUsername()
 -- not suppose to SQL form, mike to add
 
 -- addRevocation()
-INSERT INTO tbl_key_revoke (revoke_id, signature)
-VALUES (null, 'sigVar');
+--Ported
 
 -- getRevocations()
--- get the whole revocation list
-SELECT signature, time
-FROM tbl_key_revoke
+--Ported
 
 -- get only selected through specific time [I don't know if this is going to be used, but what the hell]
 -- getTimedRevocations
-SELECT signature
-FROM tbl_key_revoke
-WHERE time >= DATETIME('1YYY-MM-DD HH:MM:SS')
-AND time <= DATETIME('2YYY-MM-DD HH:MM:SS')
--- NOTE: SQLite do not store datetime as datetime, instead it stores as string. 
+--Ported
 
 -- isRevoked()
 -- not suppose to SQL form, mike to add
 
 -- addPData()
-INSERT INTO tbl_user (user_id, username, name, birthday, sex, email, public_key)
-VALUES (null, 'userVar', 'nameVar', 'YYYY-MM-DD', 'genderVar', 'email@email.com', 'keyVar');
+--Ported
 
 -- createChat()
-INSERT INTO tbl_private_message (message_id, user_from, content)
-VALUES (null, 'userVar', 'contentVar');
+--Ported
 
 -- getChat()
 
 -- addToChat()
-INSERT INTO tbl_is_in_message (is_in_id, message_id, user_id)
-VALUES (NULL, 'messageVar', 'userVar');
+--Ported
 
 -- addPost()
-INSERT INTO tbl_wall_post (wall_id, user_from, user_to, content, signature)
-VALUES (NULL, 'userVarFrom', 'userVarTo', 'contentVar', 'sigVar');
+--Ported
 
 -- getPosts()
-SELECT user_from, user_to, content
-FROM wall_post
-WHERE wall_id = 'wallVar';
+--Ported
 
--- addComment()
--- adding new independent comment
-INSERT INTO tbl_has_comment (comment_id, comment_content, wall_id, user_id, signature, comment_comment_id)
-VALUES (NULL, 'commentVar', 'wallVar', 'userVar', 'sigVar', 'comment2Var');
+-- addComment() (Independent)
+--Ported
+
+--No Dependent Comment SQL atm
 
 -- getComments() [check further]
 -- this SQL only gets comment, does not get post.
-SELECT username, name, comment_content, time
-FROM tbl_user
-INNER JOIN tbl_has_comment
-ON tbl_user.user_id = tbl_has_comment.user_id
-WHERE wall_id = 'wallVar';
+--Ported
 
 -- addLike()
-INSERT INTO tbl_has_like (like_id, post_id, user_id, comment_id)
-VALUES (null, 'postVar', 'userVar', 'commentVar');
+--Ported
 
 -- getLikes() / countLikes() any difference?
 -- get likes from post
-SELECT COUNT(like_id)
-FROM tbl_has_like
-WHERE wall_id = 'wallVar';
+--Ported to getPostLikes
 
 -- get likes from comments
-SELECT COUNT(like_id)
-FROM tbl_has_like
-WHERE comment_id = 'commentVar';
+--Ported to getCommentLikes
 
 -- addEvent()
-INSERT INTO tbl_events (event_id, user_from, title, content, start_date, end_date)
-VALUES (null, 'userVar', 'titleVar', 'contentVar', '1YYY-MM-DD', '2YYY-MM-DD');
+--Ported
 
 -- getEvent()
 -- without showing who is invited
-SELECT tbl_user.username, tbl_user.name, tbl_events.title, tbl_events.content, tbl_events.time, tbl_events.start_date, tbl_events.end_date, tbl_events.user_from
-FROM tbl_user
-INNER JOIN tbl_events
-ON tbl_user.user_id = tbl_events.user_from 
-WHERE event_id = 'eventVar';
+--Ported
 
 -- showing who is invited
 -- TODO
 -- tables involved events, is_invited, user, category
-SELECT tbl_events.title, tbl_events.content, tbl_events.time, tbl_events.start_date, tbl_events.end_date, tbl_events.from, tbl_user.username, tbl_user.name
-FROM tbl_events
-INNER JOIN tbl_is_invited
-ON tbl_events.event_id = tbl_is_invited.event_id
-INNER JOIN tbl_user
-ON tbl_is_invited.user_id = tbl_user.user_id
-INNER JOIN tbl_is_in_category
-ON tbl_user.user_id = tbl_is_in_category.user_id
-INNER JOIN tbl_category
-ON tbl_is_in_category.category_id = tbl_category.category_id
-WHERE event_id = 'eventVar';
+--Ported to getEventWithInvites
 
 -- acceptEvent()
-UPDATE tbl_is_invited
-SET decision = '1'
-WHERE user_id = 'userVar';
+--Ported
 
 -- declineEvent()
-UPDATE tbl_is_invited
-SET decision = '0'
-WHERE user_id = 'userVar';
+--Ported
 
 -- addKey()
 
 
 -- getKey() [not sure if this is right]
-SELECT public_key
-FROM tbl_user
-WHERE username = 'userVar';
+--Ported
 
 -- getName()
 -- getting name through public key
-SELECT username, name
-FROM tbl_user
-WHERE public_key = 'keyVar';
+--Ported
 
 -- addCategory()
-INSERT INTO tbl_category (category_id, name)
-VALUES (null, 'nameVar');
+--Ported
 
 -- addToCategory()
-INSERT INTO tbl_is_in_category (is_in_id, category_id, user_id)
-VALUES (null, 'categoryVar', 'userVar');
+--Ported
 
 ---------------
 -- things to do
