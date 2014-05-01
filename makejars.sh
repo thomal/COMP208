9b220c1377f39fe3e2de175bfaa84c9b27c4d5e3
louisprince <lprince.tds@gmail.com>
2014-05-01 19:58:51 +0100

    Cleaned up makejars.sh

--- makejars.sh
+++ makejars.sh
@@ -4,11 +4,13 @@
 # are in the same directory you want your output to go/ where you want to take
 # your input from. That's why this file uses ../ so much.
 
-# Make sure class files are up to date
+# Make sure all of the class files are up to date
 make clean
 make
 
-###Create the client jar###
+#########################################
+##########Create the client jar##########
+#########################################
 
 # Pack Turtlenet into a War file
 mkdir -p build
@@ -33,7 +35,9 @@ cd ..
 rm -rf build
 rm embedded.war
 
-###Create the remote server jar#### Setup the build directory
+################################################
+##########Create the remote server jar##########
+################################################
 
 # Setup directories
 mkdir build
@@ -42,6 +46,7 @@ mkdir ballmerpeak
 mkdir ./ballmerpeak/turtlenet
 mkdir ./ballmerpeak/turtlenet/remoteserver
 mkdir ./ballmerpeak/turtlenet/shared
+
 # Copy the class files we need to the build directory
 cp ../src/ballmerpeak/turtlenet/remoteserver/Server.class ./ballmerpeak/turtlenet/remoteserver/Server.class
 cp ../src/ballmerpeak/turtlenet/remoteserver/Session.class ./ballmerpeak/turtlenet/remoteserver/Session.class
@@ -49,6 +54,7 @@ cp ../src/ballmerpeak/turtlenet/remoteserver/Hasher.class ./ballmerpeak/turtlene
 cp ../src/ballmerpeak/turtlenet/shared/Conversation.class ./ballmerpeak/turtlenet/shared/Conversation.class
 cp ../src/ballmerpeak/turtlenet/shared/Message.class ./ballmerpeak/turtlenet/shared/Message.class
 cp ../src/ballmerpeak/turtlenet/shared/Tokenizer.class ./ballmerpeak/turtlenet/shared/Tokenizer.class
+
 # Create a jar out of the class files
 jar cmf ../src/ballmerpeak/turtlenet/remoteserver/MANIFEST.MF remoteserver.jar *
 # Move the jar to the jars folder
